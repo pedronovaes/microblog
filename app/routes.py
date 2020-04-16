a7e9e5e87ed650e9dae1dd1fcb98d29d994f0a37
pedronovaes <pedronovaesmelo@gmail.com>
2020-04-15 22:26:11 -0300

    Adding fake posts in view function

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,22 @@
 # Routes are the different URLs that the application implements.
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'Pedro'}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
