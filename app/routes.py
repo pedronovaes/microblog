176b5daf3194e9422fe49f8f5a379de141ceb43e
pedronovaes <pedronovaesmelo@gmail.com>
2020-04-21 15:21:55 -0300

    Recording the last visit time

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 @app.route('/')
@@ -89,3 +90,10 @@ def user(username):
     ]
 
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
