b8c829ade88616568894aa13a24bc58aeb20f74e
pedronovaes <pedronovaesmelo@gmail.com>
2020-05-06 00:49:18 -0300

    Displaying all blog posts and making it easier to find users to follow

--- app/routes.py
+++ app/routes.py
@@ -24,16 +24,7 @@ def index():
 
         return redirect(url_for('index'))
 
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    posts = current_user.followed_posts().all()
 
     return render_template('index.html', title='Home Page', form=form, posts=posts)
 
@@ -167,3 +158,11 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+
+    return render_template('index.html', title='Explore', posts=posts)
