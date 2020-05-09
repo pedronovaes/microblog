a244d82e19f7d12a792e0324b6daeef8eec3f4d9
pedronovaes <pedronovaesmelo@gmail.com>
2020-05-09 19:58:21 -0300

    Adding page navigation

--- app/routes.py
+++ app/routes.py
@@ -24,9 +24,15 @@ def index():
 
         return redirect(url_for('index'))
 
-    posts = current_user.followed_posts().all()
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
 
-    return render_template('index.html', title='Home Page', form=form, posts=posts)
+    return render_template('index.html', title='Home Page', form=form,
+                           posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -85,12 +91,19 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
 
-    return render_template('user.html', user=user, posts=posts)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+
+    next_url = url_for('user', username=username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=username, page=posts.prev_num) \
+        if posts.has_prev else None
+
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.before_request
@@ -163,6 +176,12 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
 
-    return render_template('index.html', title='Explore', posts=posts)
+    return render_template('index.html', title='Explore', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
